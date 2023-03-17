5d2fdbbb7066213a43868af21bd8169a2320b7b3
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-03-17 23:40:37 +0530

    Added : Pattern

---
+++ src/UserRegistration.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("User registration problem");
+        Scanner sc = new Scanner(System.in);
+        UserRegistrationInput userRegistration = new UserRegistrationInput();
+
+        System.out.println("Enter first name ");
+        String firstName = sc.next();
+        if (userRegistration.isFirstNameValid(firstName))
+            System.out.println("Valid first name");
+        else
+            System.out.println("Invalid first name");
+    }
+}
