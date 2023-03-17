1a740d3fa0f554825dedd8967a4a8bffb0a1142d
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-03-17 23:02:49 +0530

    Initial commit

---
+++ src/UserResgistration.java
@@ -0,0 +1,13 @@
+public class UserResgistration {
+
+    public static void main(String[] args) {
+        String firstName, lastName, email, password, phoneNumber;
+
+        Pattern pattern = new Pattern();
+        pattern.isValidFirstName();
+        firstName = pattern.getFirstName();
+        System.out.println("First Name : " + firstName);
+
+    }
+
+}
